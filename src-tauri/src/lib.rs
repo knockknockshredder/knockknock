b0d8336e7d68ba75f5e7cf126bf67daa8a67797e
PVLPM <andrey.kovalyov@gmail.com>
2026-06-19 22:11:57 +0300

    feat: add Magic UI Terminal, Phosphor icons, fonts, and Tauri dialog plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,7 @@ mod shredder;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![
             commands::shred::shred_files,
             commands::shred::get_algorithms,
